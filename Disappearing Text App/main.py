c9daaf402338fe1b15e8afd60c885cf94465a22d
Sahil Sharma <85671637+Sam6900@users.noreply.github.com>
2023-02-04 19:17:15 +0530

    Add start screen

--- Disappearing Text App/main.py
+++ Disappearing Text App/main.py
@@ -1,8 +1,8 @@
 from customtkinter import *
 
-set_widget_scaling(1.4)
+set_widget_scaling(1.3)
 win = CTk()
-win.geometry("800x600")
+win.geometry("750x600")
 win.title("Disappearing Text App")
 win.config(padx=20, pady=20)
 
@@ -12,19 +12,20 @@ end_frame = CTkFrame(win)
 
 start_frame.pack(fill=BOTH, expand=True)
 title_label = CTkLabel(start_frame, text="The Disappearing Text App", font=("", 34, "bold"))
-title_label.pack(pady=80)
+title_label.pack(pady=70)
 
-time_limit_label_a = CTkLabel(start_frame, text="Set time limit", font=("", 15, "normal"))
-time_limit_label_a.pack()
+time_limit_frame = CTkFrame(start_frame)
+time_limit_frame.pack()
 
-time_limit = CTkEntry(start_frame)
-time_limit.pack()
+time_limit_label_a = CTkLabel(time_limit_frame, text="Set time limit : ", font=("", 15, "normal"))
+time_limit_label_a.pack(side=LEFT, padx=(100, 10), pady=10)
+time_limit = CTkEntry(time_limit_frame, width=40)
+time_limit.pack(side=LEFT)
 time_limit.focus()
-
-time_limit_label_b = CTkLabel(start_frame, text="minutes", font=("", 15, "normal"))
-time_limit_label_b.pack()
+time_limit_label_b = CTkLabel(time_limit_frame, text="minutes", font=("", 15, "normal"))
+time_limit_label_b.pack(side=LEFT, padx=(10, 120))
 
 start_btn = CTkButton(start_frame, text="Start")
-start_btn.pack(pady=70)
+start_btn.pack(pady=(90, 0))
 
 win.mainloop()
