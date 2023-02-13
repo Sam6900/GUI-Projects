84b959231c8c959149b867e515ebd19d8e317667
Sahil Sharma <85671637+Sam6900@users.noreply.github.com>
2023-02-13 20:18:09 +0530

    Add start game

--- T-rex run game automation/main.py
+++ T-rex run game automation/main.py
@@ -1,1 +1,13 @@
+import pyautogui
+import time
 
+
+def jump():
+    pyautogui.press('space')
+
+    
+# Start the game    
+width, height = pg.size()
+pyautogui.click(width / 2 - 100, height / 2)
+pyautogui.press("space")
+    
