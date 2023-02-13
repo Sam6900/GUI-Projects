1ad176d951cb36c98e70be4a6d1a37356bb37c03
Sahil Sharma <85671637+Sam6900@users.noreply.github.com>
2023-02-13 20:18:51 +0530

    Add jump on obstacle detection

--- T-rex run game automation/main.py
+++ T-rex run game automation/main.py
@@ -10,4 +10,19 @@ def jump():
 width, height = pg.size()
 pyautogui.click(width / 2 - 100, height / 2)
 pyautogui.press("space")
+  
+while True:
+    # check if the T-Rex has stopped
+    if pyautogui.locateOnScreen('trex_stop.png'):
+        break
+        
+    t_rex_position = pyautogui.locateOnScreen('trex_run.png') 
+    obstacle_small = pyautogui.locateOnScreen('obstacle_small.png')
+    obstacle_large = pyautogui.locateOnScreen('obstacle_large.png')
     
+    if obstacle_small and obstacle_small[0] - trex_pos[0] < 100:
+        pg.press("space")
+    if obstacle_large and obstacle_large[0] - trex_pos[0] < 100:
+        pg.press("space")
+            
+    time.sleep(0.5)
